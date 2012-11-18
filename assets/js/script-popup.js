5ca558fe34f99af4100936addc17dc1e39168006
Evan Willhite <ewillhite@centresource.com>
2012-11-18 15:18:11 -0600

    scrolling fix2

--- assets/js/script-popup.js
+++ assets/js/script-popup.js
@@ -37,7 +37,9 @@ $(function() {
 
   // Fake 'click' when user hits the end of page
   $(window).scroll(function() {
-    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+    if (  document.documentElement.clientHeight +
+      $(document).scrollTop() >= document.body.offsetHeight )
+    {
       $('button').click();
       popUp();
     }
