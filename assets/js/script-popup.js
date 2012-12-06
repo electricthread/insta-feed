f5f668c8b2f896c043b7a132e8073f8bf5cbe1aa
Evan Willhite <ewillhite@centresource.com>
2012-12-05 21:49:33 -0600

    fix for iOS infinite scroll3

--- assets/js/script-popup.js
+++ assets/js/script-popup.js
@@ -45,7 +45,7 @@ $(function() {
 //     );
 // }
   // Fake 'click' when user hits the end of page
-  $(window).scroll(function() {
+  $(document).scroll(function() {
     if ($(window).scrollTop() + $(window).height() >= $('body').height()) {
       $('button').click();
     }
